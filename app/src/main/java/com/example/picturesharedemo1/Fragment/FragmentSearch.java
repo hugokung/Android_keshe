665e70335672c7ac6adeea403be3a16578083c7e
junmingGong <hugokungzdh212@163.com>
2020-06-18 17:15:34 +0800

    修改了FragmentSearch里加载页面的方式,以及修复不能拍照上传的bug

--- app/src/main/java/com/example/picturesharedemo1/Fragment/FragmentSearch.java
+++ app/src/main/java/com/example/picturesharedemo1/Fragment/FragmentSearch.java
@@ -36,27 +36,46 @@ public class FragmentSearch extends Fragment {
     private SwipeRefreshLayout srlayout;
     private HomeAdapter findAdapter;
     private FindAdapter findAdapter2;
-    List<Post> data= new ArrayList<>();
+    private List<Post> data = new ArrayList<>();
     private List<Post> like_data = new ArrayList<>();
     private List<Post> collect_data = new ArrayList<>();
+
     private Handler handler;
     class My_handler extends Handler{
         @Override
         public void handleMessage(Message msg){
-            if(msg.obj=="success"){
+
+            if(msg.what == 0){
+                data.clear();
+                data = (List<Post>) msg.obj;
+            }
+            else if(msg.what == 2){
+                like_data.clear();
+                like_data = (List<Post>) msg.obj;
+            }
+            else if(msg.what == 1){
+                collect_data.clear();
+                collect_data = (List<Post>) msg.obj;
                 findAdapter2 = new FindAdapter(getActivity(),data,like_data,collect_data);
                 rv.setLayoutManager(new LinearLayoutManager(getActivity()));
                 rv.setAdapter(findAdapter2);
             }
+            /*if(msg.obj=="success"){
+                findAdapter2 = new FindAdapter(getActivity(),data,like_data,collect_data);
+                rv.setLayoutManager(new LinearLayoutManager(getActivity()));
+                rv.setAdapter(findAdapter2);
+            }*/
         }
     }
+
+    @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search,container,false);
     }
 
     @Override
-    public void onActivityCreated( Bundle savedInstanceState) {
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
         initView();
@@ -70,6 +89,7 @@ public class FragmentSearch extends Fragment {
             }
         });
     }
+
     private void Refresh() {
         BmobQuery<Post> po = new BmobQuery<Post>();
         po.order("-createdAt");
@@ -78,11 +98,25 @@ public class FragmentSearch extends Fragment {
         po.findObjects(new FindListener<Post>() {
             @Override
             public void done(List<Post> list, BmobException e) {
-              //  srlayout.setRefreshing(false);
+                //srlayout.setRefreshing(false);
                 if(e==null){
-                    data.clear();
-                    data=list;
+                    //data.clear();
+                    //data=list;
+
+                    handler = new My_handler();
+                    Message msg = new Message();
+                    msg.what = 0;
+                    msg.obj = list;
+                    handler.sendMessage(msg);
+
                     init_like_list();
+//                    findAdapter = new HomeAdapter(getActivity(),data,like_data,collect_data);
+//                    //findAdapter2 = new FindAdapter(getActivity(),data);
+//                    rv.setLayoutManager(new LinearLayoutManager(getActivity()));
+//                    rv.setAdapter(findAdapter);
+//                    BmobFile test_img = list.get(0).getImage();
+//                    String test_img_url = test_img.getUrl();
+//                    Toast.makeText(getActivity(), "图片地址:"+test_img_url, Toast.LENGTH_SHORT).show();
                 }
                 else{
                     srlayout.setRefreshing(false);
@@ -95,8 +129,8 @@ public class FragmentSearch extends Fragment {
     private void initView() {
         rv = getActivity().findViewById(R.id.find_recyclerview);
         srlayout = getActivity().findViewById(R.id.find_swipe);
-
     }
+
     private void init_like_list(){
 
         BmobUser this_user = BmobUser.getCurrentUser(BmobUser.class);
@@ -108,10 +142,17 @@ public class FragmentSearch extends Fragment {
             @Override
             public void done(List<Post> list, BmobException e) {
                 if(e==null){
-                    like_data.clear();
-                    like_data = list;
+                    //like_data.clear();
+                    //like_data = list;
                     Integer list_num = list.size();
-                    //Toast.makeText(getActivity(), "获取点赞信息成功"+list_num.toString(), Toast.LENGTH_SHORT).show();
+
+                    handler = new My_handler();
+                    Message  msg = new Message();
+                    msg.what = 2;
+                    msg.obj = list;
+                    handler.sendMessage(msg);
+                    init_collect_list();
+                    Toast.makeText(getActivity(), "获取点赞信息成功"+list_num.toString(), Toast.LENGTH_SHORT).show();
                 }
                 else{
                     srlayout.setRefreshing(false);
@@ -119,20 +160,50 @@ public class FragmentSearch extends Fragment {
                 }
             }
         });
+        /*
         BmobQuery<Post> query_collect = new BmobQuery<>();
         query_collect.addWhereRelatedTo("user_collect",new BmobPointer(me));
         query_collect.findObjects(new FindListener<Post>() {
             @Override
             public void done(List<Post> list, BmobException e) {
                 if(e==null){
-                    collect_data.clear();
-                    collect_data = list;
+                    //collect_data.clear();
+                    //collect_data = list;
                     handler = new My_handler();
                     Message meg = new Message();
                     meg.obj="success";
+                    meg.obj = list;
+                    meg.what = 1;
                     handler.sendMessage(meg);
                     srlayout.setRefreshing(false);
-                  // Toast.makeText(getActivity(), "获取收藏信息成功"+list.size(), Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(getActivity(), "获取收藏信息成功", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(getActivity(), "获取收藏信息失败"+e, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });*/
+    }
+    private void init_collect_list(){
+        BmobUser this_user = BmobUser.getCurrentUser(BmobUser.class);
+        User me = new User();
+        me.setObjectId(this_user.getObjectId());
+        BmobQuery<Post> query_collect = new BmobQuery<>();
+        query_collect.addWhereRelatedTo("user_collect",new BmobPointer(me));
+        query_collect.findObjects(new FindListener<Post>() {
+            @Override
+            public void done(List<Post> list, BmobException e) {
+                if(e==null){
+                    //collect_data.clear();
+                    //collect_data = list;
+                    handler = new My_handler();
+                    Message meg = new Message();
+                    meg.obj="success";
+                    meg.obj = list;
+                    meg.what = 1;
+                    handler.sendMessage(meg);
+                    srlayout.setRefreshing(false);
+//                    Toast.makeText(getActivity(), "获取收藏信息成功", Toast.LENGTH_SHORT).show();
                 }
                 else{
                     Toast.makeText(getActivity(), "获取收藏信息失败"+e, Toast.LENGTH_SHORT).show();
